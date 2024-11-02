0a742c4928552d37a76fdd5b3aca5794bda98973
vvuukk <vucicevic9@gmail.com>
2024-11-02 02:18:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,52 @@
 const myImage = document.querySelector("img");
 
+const pozadine = document.querySelector("h1");
+
+let uvod = myImage.getAttribute("src").split("/")[1].split(".")[0];
+pozadine.innerHTML += uvod;
+
+let br = 1;
+
 myImage.onclick = () => {
-
-   const mySrc = myImage.getAttribute("src");
-    if(mySrc === "slika.jpg")
-        myImage.setAttribute("src","pigs.jpg");
-    if(mySrc ==="pigs.jpg")
+    const mySrc = myImage.getAttribute("src");
+    let ime = "";
+    mySrc.split("/")[1].split(".")[0];
+    if(mySrc === "pigs.jpg"){
+        myImage.setAttribute("src","hyrax.jpg");
+        ime = "hyrax";
+    }  
+    if(mySrc ==="hyrax.jpg"){
         myImage.setAttribute("src","bugs.jpg");
-    if(mySrc === "bugs.jpg")
-        myImage.setAttribute("src","slika.jpg");
-}
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName(){
-    const myName = prompt("Please enter your name:");
-    if(!myName){
-        setUserName();
-    }else{
-        localStorage.setItem("name",myName);
-        myHeading.textContent=`Mozzila is cool, ${myName}`; 
+        ime = "bugs";
+    }
+        
+    if(mySrc === "bugs.jpg"){
+        myImage.setAttribute("src","pigs.jpg");
+        ime = "pigs";
+    }
+        
+    if(!pozadine.innerHTML.includes(ime)){
+        pozadine.innerHTML += ", ";
+        pozadine.innerHTML +=ime;
+        br++;
+    }
+        
+    if(pozadine.innerHTML.charAt(pozadine.innerHTML.length-1)==','){
+        let pozz = pozadine.innerHTML.substring(0,pozadine.innerHTML.length-1);
+        pozadine.innerHTML = pozz;
     }
     
 }
 
-if (!localStorage.getItem("name")){
-    setUserName();
-}
-else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozzila is cool, ${storedName}`;   
-}
+let myButton = document.querySelector("button");
 
 myButton.onclick = () => {
-    setUserName();
+    notifikacija();
+}
+
+function notifikacija(){
+    if(br>1 && br<5)
+        alert("Trenutno ima "+br+" pozadine!");
+    else
+        alert("Trenutno ima "+br+" pozadina!");
 }
